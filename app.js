b8c837bbabf621cb749ae4d83ef3e024fc9de398
Tristan <tristanw3@outlook.com>
2020-01-25 23:19:48 +1100

    second

--- app.js
+++ app.js
@@ -5,69 +5,110 @@ var graphics = canvas.getContext("2d");
 var chart = {
   // data: [70, 100, 30, 200, 125],
   // data: [700, 1000, 300, 2000, 1250],
-  data: [500, 1000, 1500, 2000, 100],
-  labels: ["people", "spiders", "mutants", "ants", "oranges"],
-  colors: ["red", "green", "blue", "orange", "pink"],
+  // data: [500, 1000, 1500, 2000, 200],
+  data: [1359, 5, 5, 4, 3, 3],
+  // data: [5, 5, 4, 3, 3],
+  // labels: ["people", "spiders", "mutants", "ants", "oranges"],
+  labels: ["China", "Hong Kong", "Thailand", "Australia", "France", "Japan"],
+  // labels: ["Hong Kong", "Thailand", "Australia", "France", "Japan"],
+
+  colors: ["red", "green", "blue", "orange", "pink", "cyan"],
+  // colors: ["green", "blue", "orange", "pink", "cyan"],
   barWidth: 30
 };
 
-chart.maxHeight = Math.max(...chart.data);
 
-function DrawBarChart(chart) {
-  // clear the screen for the next frame.
-  graphics.clearRect(0, 0, canvas.width, canvas.height);
-  // fill rect (position x,position y, width, height)
+function run(china) {
+  if (china === "yes") {
+    chart.data = [1359, 5, 5, 4, 3, 3]
+    chart.labels = ["China", "Hong Kong", "Thailand", "Australia", "France", "Japan"]
+    chart.colors = ["red", "green", "blue", "orange", "pink", "grey"]
+  } else {
+    chart.data = [5, 5, 4, 3, 3];
+    chart.labels = ["Hong Kong", "Thailand", "Australia", "France", "Japan"];
+    chart.colors = ["green", "blue", "orange", "pink", "grey"];
+  }
 
-  graphics.font = "16px sans-serif";
 
-  graphics.strokeStyle = "black";
-  graphics.beginPath();
-  graphics.moveTo(60, 0);
-  graphics.lineTo(60, canvas.height - 40);
-  graphics.stroke();
+  let mh = Math.max(...chart.data);
+  let ch = canvas.height - 50;
 
-  //Create y-axis
-  let mh = chart.maxHeight;
-  let ch = canvas.height;
-  yLabels = [1 * mh, 0.75 * mh, 0.5 * mh, 0.25 * mh];
+  function DrawBarChart(chart) {
+    // clear the screen for the next frame.
+    graphics.clearRect(0, 0, canvas.width, canvas.height);
+    // fill rect (position x,position y, width, height)
 
-  yLabels.forEach((e, i) => {
-    let tickPosition = ch * (i / 4) - 40;
+    graphics.font = "16px Arial";
+
+    graphics.strokeStyle = "black";
     graphics.beginPath();
-    graphics.moveTo(60, tickPosition);
-    graphics.lineTo(70, tickPosition);
+    graphics.moveTo(60, 19);
+    graphics.lineTo(60, ch + 20);
     graphics.stroke();
 
-    graphics.fillStyle = "black";
-    let labelPosition = tickPosition + 6;
-    graphics.fillText(e, 20, labelPosition);
-  });
+    //Create y-axis
 
-  chart.data.forEach((ele, ind) => {
-    let x = 100 * ind + 80;
-    let w = 90;
-    let h = (ele / mh) * canvas.height;
-    let y = canvas.height - h - 40;
+    let yLabels = [1 * mh, 0.75 * mh, 0.5 * mh, 0.25 * mh, 0];
 
-    graphics.globalAlpha = 0.4;
-    graphics.fillStyle = chart.colors[ind];
-    graphics.fillRect(x, y, w, h);
+    yLabels.forEach((e, i) => {
+      let tickPosition = ch * (i / 4) + 20;
+      graphics.beginPath();
+      graphics.moveTo(60, tickPosition);
+      graphics.lineTo(70, tickPosition);
+      graphics.stroke();
 
-    graphics.globalAlpha = 1;
-    graphics.strokeStyle = chart.colors[ind];
-    graphics.strokeRect(x, y, w, h);
+      graphics.fillStyle = "black";
+      let labelPosition = tickPosition + 6;
 
-    graphics.fillText(chart.labels[ind], x + 20, canvas.height - 16);
-  });
 
-  // when we finish rendering, ask the browser to render us again
-  requestAnimationFrame(DrawBarChart);
+      let numPosition;
+      if (e >= 0) {
+        numPosition = 45;
+      }
+      if (e >= 10) {
+        numPosition = 37;
+      }
+      if (e >= 100) {
+        numPosition = 27;
+      }
+      if (e >= 1000) {
+        numPosition = 17;
+      }
+
+
+      graphics.fillText(Math.round(e), numPosition, labelPosition);
+    });
+
+    chart.data.forEach((ele, ind) => {
+      let w = 500 / chart.data.length;
+      let x = (w + 10) * ind + 80;
+      let h = (ele / mh) * ch;
+      let y = ch - h + 20;
+
+      graphics.globalAlpha = 0.4;
+      graphics.fillStyle = chart.colors[ind];
+      graphics.fillRect(x, y, w, h);
+
+      graphics.globalAlpha = 1;
+      graphics.strokeStyle = chart.colors[ind];
+      graphics.strokeRect(x, y, w, h);
+
+      graphics.fillText(chart.labels[ind], x + 5, ch + 37);
+    });
+
+    // when we finish rendering, ask the browser to render us again
+
+    requestAnimationFrame(DrawBarChart);
+  }
+
+  function RenderLoop() {
+    DrawBarChart(chart);
+    requestAnimationFrame(RenderLoop);
+  }
+
+  // start the render loop
+  RenderLoop();
+
 }
 
-function RenderLoop() {
-  DrawBarChart(chart);
-  requestAnimationFrame(RenderLoop);
-}
-
-// start the render loop
-RenderLoop();
+run("yes");
